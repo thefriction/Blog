bc5957a774e7debf57358c1b5a673de27b9b6ddf
thefriction <adityaaswal98@gmail.com>
2020-06-13 23:36:50 +0530

    Added mongoDB via mongoose

--- app.js
+++ app.js
@@ -3,17 +3,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-;
+const mongoose = require("mongoose");
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser:true});
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const blogsSchema = new mongoose.Schema({
+  title:String,
+  content:String
+});
+
+const Blog = mongoose.model("Blog", blogsSchema);
+
 var arr = [];
 
 app.get("/contact",function(req,res){
@@ -39,47 +48,54 @@ app.get("/compose",function(req,res){
 
 });
 app.post("/compose", function(req,res){
-  var post = {
-    title:req.body.postTitle,
-    content:req.body.postBody     
-  }
   
-  arr.push(post);
+   const tmp = new Blog({
+    title:req.body.postTitle,
+    content:req.body.postBody
+  });
+
+  tmp.save(function(err){ //callback to the save method to only redirect to the home page once save is complete with no errors.
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+    });
   res.redirect("/");
 });
 
-app.post("/", function(req,res){
-  console.log(res); 
-});
 app.get("/",function(req,res){
-  res.render("home",
-  {
-    para1:homeStartingContent,
-    articles:arr
+  Blog.find({}, function(err, foundBlogs){
+    res.render("home",
+    {
+        listTitle:"HELLO WORLD",
+        blogs:foundBlogs
+    });
   });
-  
 });
 
-app.get("/posts/:postName",function(req,res){
-  var str = req.params.postName.replace(' ', '-').toLowerCase() ;
-  console.log(str);
-  
-  arr.forEach(function(posts){
-    
-    
-    if(posts.title ===  str)
+app.get("/posts/:p_id",function(req,res){
+  console.log(req.params.p_id);
+  Blog.findOne({_id:req.params.p_id}, function(err, foundBlog){
+    if(!foundBlog)
+    {
+      //send to compose a blog
+      res.render("compose",
+      {        
+        heading:"No match to you requested post, might write on as well"
+      })
+    }
+    else
     {
       res.render("post",
       {
-        title:posts.title,
-        body:posts.content
-      });
-    }      
+          blog:foundBlog
+       });
+     }
   });
-  res.send("NOT FOUND");
-  
-
 });
+ 
 
 
 
